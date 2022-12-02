0a430d7e3fa1d040ffc35d5ab4b618cee6d71cf8
Abhishek Anantharam <abhishekanantharam123@gmail.com>
2022-12-03 02:30:15 +0530

    fast api setup

---
+++ ml/main.py
@@ -0,0 +1,25 @@
+import json
+from pydantic import BaseModel
+from fastapi import FastAPI
+from keywordSample import extract_keywords
+from comparison import score
+app = FastAPI()
+
+
+class WebData(BaseModel):
+    data: str
+
+
+@app.post("/keywords/")
+def extractKeyword(data: WebData):
+    keywords = extract_keywords(data.data)
+    return {"keywords": keywords}
+
+
+@app.post("/compare/")
+def compareTags(data: WebData):
+
+    d = json.loads(data.data)
+    print(d)
+    s = score(d)
+    return {"score": s}
